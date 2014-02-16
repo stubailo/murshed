a232d088fd64a3e855a96498f3cfa2a0259a2c63
Sashko Stubailo <sashko@mit.edu>
2014-02-16 12:18:21 +0400

    Added title and also permissions

--- collections.js
+++ collections.js
@@ -5,4 +5,22 @@ Answers = new Meteor.Collection("answers");
 if (Meteor.isServer) {
   Questions._ensureIndex({from: 1, to: 1}, {unique: 1});
   Landmarks._ensureIndex({ lngLat : "2d" });
+
+  Landmarks.allow({
+    insert: function () {
+      return true;
+    }
+  });
+
+  Questions.allow({
+    insert: function () {
+      return true;
+    }
+  });
+
+  Answers.allow({
+    insert: function () {
+      return true;
+    }
+  });
 }
